f2d6486a8b2c1432d048d8398e21e86fd777fe57
Maksim Tropnikov <makstropnikov@gmail.com>
2021-09-02 00:29:19 +0700

    feat: add and remove tasks working

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const addForm = document.querySelector('.add-task-form');
 const taskTemplate = document.querySelector('.task-template').content;
 
-const handleTaskSubmit = function (e) {
-    e.preventDefault();
-    const textInput = addForm.elements[0].value;
-    if (textInput) {
-        const listItem = taskTemplate.cloneNode(true);
-        listItem.querySelector('.task').textContent = textInput;
-        document.querySelector('.task-list').append(listItem);
-        addForm.reset();
-    }
+const handleTaskDelete = (evt) => {
+  //TODO: maybe rewrite later
+  evt.target.parentNode.remove();
+};
+
+function handleTaskSubmit(e) {
+  e.preventDefault();
+  const textInput = addForm.elements[0].value;
+  if (textInput) {
+    const listItem = taskTemplate.cloneNode(true);
+    listItem.querySelector('.task').textContent = textInput;
+    listItem.querySelector('.delete-btn').addEventListener('click', (evt) => {
+      handleTaskDelete(evt);
+    });
+    document.querySelector('.task-list').append(listItem);
+    addForm.reset();
+  }
 }
+
 addForm.addEventListener('submit', handleTaskSubmit);
